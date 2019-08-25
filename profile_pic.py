e9833625cae40c8f60b71a57477d6bc0656da7d3
suryavip <suryabejibun@gmail.com>
2019-08-26 01:45:24 +0700

    remove profile pic

--- profile_pic.py
+++ profile_pic.py
@@ -4,6 +4,7 @@ from connection import FirebaseCon, MysqlCon
 from rules import Rules
 from util import saveUploadedFile, getSingleField
 import werkzeug
+import os
 
 
 class SelfProfilePic(Resource):
@@ -28,13 +29,31 @@ class SelfProfilePic(Resource):
 
         return {}, 201
 
+    def delete(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('X-idToken', required=True, help='a', location='headers')
+        args = parser.parse_args()
+
+        fbc = FirebaseCon(args['X-idToken'])
+
+        try:
+            os.remove('storage/profile_pic/{}'.format(fbc.uid))
+        except:
+            pass
+        try:
+            os.remove('storage/profile_pic/{}_thumb'.format(fbc.uid))
+        except:
+            pass
+            
+        return {}
+
 
 class ProfilePic(Resource):
     def get(self, uid):
         # uid may ended with _thumb
         mysqlCon = MysqlCon()
         parser = reqparse.RequestParser()
-        parser.add_argument('r', required=True, help='r', location='args') # requester
+        parser.add_argument('r', required=True, help='r', location='args')  # requester
         args = parser.parse_args()
 
         requester = getSingleField(mysqlCon, 'userId', 'userdata', 'userId', args['r'], default=None)
