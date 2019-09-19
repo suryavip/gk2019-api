e2e396eab21759ba192aa53da7d2e3d149027cc8
suryavip <suryabejibun@gmail.com>
2019-09-19 13:58:23 +0700

    change the help text

--- feedback.py
+++ feedback.py
@@ -10,7 +10,7 @@ class Feedback(Resource):
         mysqlCon = MysqlCon()
         parser = reqparse.RequestParser()
         parser.add_argument('X-idToken', required=True, help='a', location='headers')
-        parser.add_argument('liked', type=bool ,required=True, help='like')
+        parser.add_argument('liked', type=bool ,required=True, help='liked')
         parser.add_argument('suggestion')
         parser.add_argument('deviceModel')
         parser.add_argument('devicePlatform')
